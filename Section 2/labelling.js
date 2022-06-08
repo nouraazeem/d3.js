678f7d658aaeee785ad33aa0055e2f124ac13a85
Noura Azeem <noura.azeem@gmail.com>
2022-06-08 15:37:58 -0400

    labelling section

--- Section 2/labelling.js
+++ Section 2/labelling.js
@@ -1,7 +1,7 @@
 var w = 300;
-var h = 100;
+var h = 120;
 var padding = 2;
-var dataset = [5, 10, 14, 20, 25];
+var dataset = [5, 10, 13, 19, 21, 25, 11, 25, 22, 18, 7];
 var svg = d3.select("body").append("svg")
           .attr("width", w)
           .attr("height", h);
@@ -15,16 +15,21 @@ svg.selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
-.attr("x", function(d, i) {
-  return i * (w / dataset.length);
-})
-.attr("y", function(d) {
-  return h - (d*4);
-})
+.attr("x", function(d, i) { return i * (w / dataset.length); })
+.attr("y", function(d) { return h - (d*4); })
      .attr("width", w / dataset.length - padding)
-     .attr("height", function(d) {
-  return d*4
+     .attr("height", function(d) { return d*4
 })
-  .attr("fill", function(d) {
-  return colorPicker(d);
+  .attr("fill", function(d) { return colorPicker(d);
 });
+
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function (d) { return d;})
+.attr({
+  "text-anchor": "middle",
+  x: function(d,i) {return i * (w / dataset.length);},
+  y: function(d) { return h - (d*4); }
+})
